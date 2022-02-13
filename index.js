924a7bbf14a1416b7148f6e26aa48ced9ada73b7
bmx22c <github@bmx22c.fr>
2022-02-13 02:27:58 +0100

    code cleaning + removing cert

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const fs = require('fs')
 const express = require('express')
 const cors = require('cors')
-const https = require('https')
 
 // The developer rig uses self-signed certificates.  Node doesn't accept them
 // by default.  Do not use this in production.
@@ -11,18 +9,14 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const verboseLogging = true
 const verboseLog = verboseLogging ? console.log.bind(console) : () => { }
 
-const privateKey = fs.readFileSync('cert/key.pem')
-const certificate = fs.readFileSync('cert/cert.pem')
-const credentials = {key: privateKey, cert: certificate}
 const app = express()
-// const httpsServer = https.createServer(credentials, app)
 const port = ~~process.env.PORT || 3000
 
 app.use(cors());
 
 require('./routes/home.js')(app)
 require('./routes/physics.js')(app)
-// require('./routes/checkprofile.js')(app)
+
 app.use('/assets', express.static(process.cwd() + '/assets'))
 
 app.listen(port, () => {
